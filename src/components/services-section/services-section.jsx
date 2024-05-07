c11606842300c451c82a34900a340d89d23c7177
ijaz liaqat <ijazierpu@gmail.com>
2024-05-07 23:26:53 +0500

    grid system gifted to kaaaje

--- src/components/services-section/services-section.jsx
+++ src/components/services-section/services-section.jsx
@@ -3,10 +3,108 @@ import "./style/services-section-style.css";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { Box, CardActionArea, CardActions, IconButton } from "@mui/material";
+import {
+  Box,
+  CardActionArea,
+  CardActions,
+  Grid,
+  IconButton,
+} from "@mui/material";
 import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
 
 const ServicesSection = () => {
+  const cardDummyData = [
+    {
+      icon: (
+        <WorkOutlineIcon
+          sx={{
+            color: "#413E66",
+            fontSize: "60px",
+            color: "#FE689B",
+          }}
+          className="bg-card1-icon rounded-pill p-2"
+        />
+      ),
+      title: "Lorem Ipsum",
+      decp: `Voluptatum deleniti atque corrupti quos dolores et quas 
+      molestias excepturi sint occaecati cupiditate non provident`,
+    },
+    {
+      icon: (
+        <WorkOutlineIcon
+          sx={{
+            color: "#413E66",
+            fontSize: "60px",
+            color: "#FE689B",
+          }}
+          className="bg-card1-icon rounded-pill p-2"
+        />
+      ),
+      title: "Lorem Ipsum",
+      decp: `Voluptatum deleniti atque corrupti quos dolores et quas 
+      molestias excepturi sint occaecati cupiditate non provident`,
+    },
+    {
+      icon: (
+        <WorkOutlineIcon
+          sx={{
+            color: "#413E66",
+            fontSize: "60px",
+            color: "#FE689B",
+          }}
+          className="bg-card1-icon rounded-pill p-2"
+        />
+      ),
+      title: "Lorem Ipsum",
+      decp: `Voluptatum deleniti atque corrupti quos dolores et quas 
+      molestias excepturi sint occaecati cupiditate non provident`,
+    },
+    {
+      icon: (
+        <WorkOutlineIcon
+          sx={{
+            color: "#413E66",
+            fontSize: "60px",
+            color: "#FE689B",
+          }}
+          className="bg-card1-icon rounded-pill p-2"
+        />
+      ),
+      title: "Lorem Ipsum",
+      decp: `Voluptatum deleniti atque corrupti quos dolores et quas 
+      molestias excepturi sint occaecati cupiditate non provident`,
+    },
+    {
+      icon: (
+        <WorkOutlineIcon
+          sx={{
+            color: "#413E66",
+            fontSize: "60px",
+            color: "#FE689B",
+          }}
+          className="bg-card1-icon rounded-pill p-2"
+        />
+      ),
+      title: "Lorem Ipsum",
+      decp: `Voluptatum deleniti atque corrupti quos dolores et quas 
+      molestias excepturi sint occaecati cupiditate non provident`,
+    },
+    {
+      icon: (
+        <WorkOutlineIcon
+          sx={{
+            color: "#413E66",
+            fontSize: "60px",
+            color: "#FE689B",
+          }}
+          className="bg-card1-icon rounded-pill p-2"
+        />
+      ),
+      title: "Lorem Ipsum",
+      decp: `Voluptatum deleniti atque corrupti quos dolores et quas 
+      molestias excepturi sint occaecati cupiditate non provident`,
+    },
+  ];
   return (
     <>
       <Box className="services-section-bg-color mt-5">
@@ -21,9 +119,44 @@ const ServicesSection = () => {
           </p>
         </Box>
 
+        <Grid container gap={2} className="container">
+          {cardDummyData?.map((item) => {
+            return (
+              <Grid item xs={11.8} sm={5.8} md={3.8}>
+                <Card sx={{ maxWidth: 500 }}>
+                  <CardContent>
+                    <Box className="text-center my-3">
+                     {item?.icon}
+                    </Box>
+                  </CardContent>
+
+                  <CardActionArea>
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="div"
+                        className="fw-bold text-center montserrat-font"
+                      >
+                        {item?.title}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+
+                  <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                      {item?.decp}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+
         {/* Cards */}
-        <Box className="container mt-5">
-          {/* Card row 1 */}
+        {/* <Box className="container mt-5">
+          
           <Box className="row">
             <Box className="col-md-4 my-4 my-sm-0">
               <Card sx={{ maxWidth: 500 }}>
@@ -137,7 +270,6 @@ const ServicesSection = () => {
             </Box>
           </Box>
 
-          {/* Card row 2 */}
           <Box className="row mt-4">
             <Box className="col-md-4 my-4 my-sm-0">
               <Card sx={{ maxWidth: 500 }}>
@@ -250,7 +382,7 @@ const ServicesSection = () => {
               </Card>
             </Box>
           </Box>
-        </Box>
+        </Box> */}
       </Box>
     </>
   );
